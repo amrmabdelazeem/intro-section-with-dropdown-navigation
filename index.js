ebb4c0e5769a7f1331c982d4e442c95fb2337185
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-24 19:34:09 +0300

    Toggle menu with js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-$("#icon-menu").on("click",function(){
-    $(".nav-menu").removeClass("close-menu");
-})
+// $("#icon-menu").on("click",function(){
+//     $(".nav-menu").removeClass("close-menu");
+// })
 
-$(".icon-close").on("click", function(){
-    $(".nav-menu").addClass("close-menu");
+// $(".icon-close").on("click", function(){
+//     $(".nav-menu").addClass("close-menu");
+// })
+
+$(".toggle-menu").on("click",function(){
+    $(".nav-menu").toggleClass("close-menu");
 })
