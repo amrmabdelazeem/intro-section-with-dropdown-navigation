f2821f070f275ac5b8a3dba8bc514ffd1dd510e2
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-25 17:42:35 +0300

    Add toggle arrow heads in dropdown menus

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ $(".toggle-menu").on("click",function(){
     $(".nav-menu").toggleClass("close-menu");
     $("#overlay").toggleClass("close-menu");
 })
+
+$("summary").on("click", function(){
+    var arrowSrc = $(this).find("img").attr('src');
+    if(arrowSrc ==="./images/icon-arrow-down.svg"){
+        $(this).find("img").attr('src',"./images/icon-arrow-up.svg");
+    }else{
+        $(this).find("img").attr('src',"./images/icon-arrow-down.svg");
+    }
+})
